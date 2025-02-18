980a8b53ecb16bf0af6c52bfd1cd783a6c7d022b
vinifen <ferreiranivicius@gmail.com>
2025-02-18 14:50:47 -0300

    📝 Updated selected entries

--- src/view/appointment/CreateScreen.ts
+++ src/view/appointment/CreateScreen.ts
@@ -1,6 +1,5 @@
 import { Validation } from "../Validation";
 import PrimaryScreen from "../PrimaryScreen";
-import Router from "../../router/Router";
 
 export default abstract class CreateScreen extends Validation {
   constructor(public primaryScreen: PrimaryScreen) {
